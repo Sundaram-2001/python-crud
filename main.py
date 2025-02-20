01acfb95792cdd9ff330195ef5256331ae7caf53
Sundaram Saroj <sundaramsaroj@Sundarams-MacBook-Air.local>
2025-02-21 00:01:01 +0530

    minor changes

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
 import json
-with open('data.json','r') as file:
-    data=json.load(file)
-formatted_data=json.dumps(data,indent=4)
-print(formatted_data)
+from typing import Any
 
+file_path="data.json"
+def load_data()->list[dict]:
+    """Load JSON data from the file."""
+    try:
+        with open(file_path,'r') as file:
+            return json.load(file)
+    except (FileNotFoundError):
+        return []
+    
+def save_data(data:list[dict]):
+    """Save data to the JSON file."""
+    try:
+        with open(file_path,'w') as file:
+            json.dump(file,indent=4)
+    except (FileNotFoundError):
+        print("file not found!")
 
+def add_task()->None:
+    try:
+        data=load_data()
+        new_task={
+            "id": len(data) + 1,
+            "task": "Buy groceries",
+            "completed": "false",
+            "priority": "high",
+            "dueDate": "2024-03-20"
+        }
+        data.append(new_task)
+        save_data(data)
+        print("Task added successfully!")
+    except Exception as e:
+        print(f"Error adding task: {e}")
 
 
